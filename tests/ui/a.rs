b59328a16e3e8bc6bca73dd4cefa99d301756e52
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2023-02-07 15:13:51 -0500

    thread-test

--- tests/ui/a.rs
+++ tests/ui/a.rs
@@ -1,9 +1,20 @@
 use no_panic::no_panic;
 
-async fn a() {
-    println!("a");
+use std::thread;
+
+#[no_panic]
+fn a() {
+    thread::spawn(b);
+}
+
+async fn b() {
+    thread::spawn(c);
+}
+
+async fn c() {
+    panic!("xxx");
 }
 
 fn main() {
-    async_std::task::block_on(a());
+    a();
 }
